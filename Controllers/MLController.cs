cf2c74686e12f8114c9e5c668336c9f0d927d8ad
xjilio123 <joaquin200314@gmail.com>
2024-06-07 11:15:50 -0500

    Cambios para los datos ingresados

--- Controllers/MLController.cs
+++ Controllers/MLController.cs
@@ -13,5 +13,92 @@ using pc4_progra.Services;
 
 namespace pc4_progra.Controllers
 {
-    
+    public class MLController : Controller
+    {
+        private readonly ILogger<MLController> _logger;
+        private readonly PredictionEnginePool<ProductoRating, ProductoRatingPrediction> _model;
+        private readonly ProductoService _productoService;
+
+
+        public MLController(ILogger<MLController> logger,
+        PredictionEnginePool<ProductoRating, ProductoRatingPrediction> model,
+        ProductoService productoService)
+        {
+            _logger = logger;
+            _model = model;
+            _productoService = productoService;
+        }
+
+        public IActionResult Trending()
+        {
+            return View();
+        }
+        public IActionResult PredictTrending(int id)
+        {
+            List<(int productoId, float normalizedScore)> ratings = new List<(int productoId, float normalizedScore)>();
+            ProductoRatingPrediction prediction = null;
+
+            foreach (var producto in _productoService.getTrendingProductos())
+            {
+                // Call the Rating Prediction for each movie prediction
+                prediction = _model.Predict(new ProductoRating
+                {
+                    userid = id,
+                    productoid = producto.ProductoId // Asegúrate de que coincide con el nombre de la propiedad en ProductoRating
+                });
+
+                // Normalize the prediction scores for the "ratings" b/w 0 - 100
+                float normalizedscore = Sigmoid(prediction.Score);
+
+                // Add the score for recommendation of each movie in the trending movie list
+                ratings.Add((producto.ProductoId, normalizedscore));
+            }
+
+            ratings = ratings.OrderByDescending(r => r.normalizedScore).ToList();
+
+            ViewData["ratings"] = ratings;
+            ViewData["trendingproductos"] = _productoService.getTrendingProductos();
+            return View("Trending");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Predict(int id)
+        {
+            List<(int productoId, float normalizedScore)> ratings = new List<(int productoId, float normalizedScore)>();
+            ProductoRatingPrediction prediction = null;
+
+            foreach (var producto in _productoService.getAllProductos())
+            {
+                // Call the Rating Prediction for each movie prediction
+                prediction = _model.Predict(new ProductoRating
+                {
+                    userid = id,
+                    productoid = producto.ProductoId // Asegúrate de que coincide con el nombre de la propiedad en ProductoRating
+                });
+
+                // Normalize the prediction scores for the "ratings" b/w 0 - 100
+                float normalizedscore = Sigmoid(prediction.Score);
+
+                // Add the score for recommendation of each movie in the trending movie list
+                ratings.Add((producto.ProductoId, normalizedscore));
+            }
+
+            ratings = ratings.OrderByDescending(r => r.normalizedScore).ToList();
+
+            ViewData["ratings"] = ratings;
+            ViewData["productos"] = _productoService.getAllProductos();
+            return View("Index");
+        }
+        public float Sigmoid(float x)
+        {
+            return (float) (100/(1 + Math.Exp(-x)));
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
 }
--- DataStructures/ProductoRating.cs
+++ DataStructures/ProductoRating.cs
@@ -5,12 +5,5 @@ using System.Threading.Tasks;
 
 namespace pc4_progra.DataStructures
 {
-    public class ProductoRating
-    {
-        public float userid;
-
-        public float productoid; // Cambia el tipo de datos a float o el tipo esperado por el modelo
-
-        public bool Label;
-    }
+    
 }
