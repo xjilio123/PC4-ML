da43145e16754d018e3c08485eb325ab5bd270d7
xjilio123 <joaquin200314@gmail.com>
2024-06-07 11:17:17 -0500

    creacion de los productos

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -7,8 +7,6 @@ namespace pc4_progra.Models
 {
     public class Producto
     {
-        public int ProductoId { get; set; }
-        public string ProductoNombre { get; set; }
-        public bool liked;
+        
     }
 }
