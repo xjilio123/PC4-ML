f2eedbc4c26bc7b5aa9f3562d56680274d3f3d0a
xjilio123 <joaquin200314@gmail.com>
2024-06-07 11:52:29 -0500

    cambios en productos

--- Data/ProductoRating.cs
+++ Data/ProductoRating.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace pc4_progra.DataStructures
+namespace pc4_progra.Data
 {
     public class ProductoRating
     {
